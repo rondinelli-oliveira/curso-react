86e6fc9a4dd47dad292e43e1f89daf5c6a45b50b
rondinelli-oliveira <rondinelliads@gmail.com>
2020-10-06 03:12:29 -0300

    Aula-06 - Debugando o NodeJS.

--- gobarber-backend/src/routes/index.ts
+++ gobarber-backend/src/routes/index.ts
@@ -2,8 +2,22 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello Rondinelli!' }),
-);
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
 
 export default routes;
+// const routes = Router();
+
+// routes.get('/', (request, response) =>
+//   response.json({ message: 'Hello Rondinelli!' }),
+// );
+
+// export default routes;
